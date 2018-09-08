40e880bbefe45b62dba7b408190c2f63a54b0ac9
Bryan Chu <thisisbryanchu@gmail.com>
2018-09-08 00:53:06 -0700

    Add good and bad word constants

---
+++ src/Constants.js
@@ -0,0 +1,3 @@
+export const GOOD_WORDS = new Set(["smashmouth"]);
+
+export const BAD_WORDS = new Set(["poo"]);
--- src/Game.js
+++ src/Game.js
@@ -2,19 +2,35 @@ import React, { Component } from "react";
 import WordBar from "./WordBar";
 import Word from "./Word";
 
+import { GOOD_WORDS, BAD_WORDS } from "./Constants";
+
 class Game extends Component {
   state = {
-    words: []
+    goodWords: [],
+    badWords: [],
+    toastText: ""
   };
 
-  addWord = word => this.setState({ words: [...this.state.words, word] });
+  addGoodWord = base => word => !base.includes(word) && this.setState({ goodWords: [...base, word] });
+
+  addBadWord = base => word => !base.includes(word) && this.setState({ badWords: [...base, word] });
+
+  addWord = word => {
+    if (GOOD_WORDS.has(word)) {
+      this.addGoodWord(this.state.goodWords)(word);
+    } else if (BAD_WORDS.has(word)) {
+      this.addBadWord(this.state.badWords)(word);
+    }
+  };
 
   render() {
-    const words = this.state.words.map(w => <Word word={w} />);
+    const goodWords = this.state.goodWords.map(w => <Word word={w} />);
+    const badWords = this.state.badWords.map(w => <Word word={w} />);
     return (
       <div className="game">
         <WordBar addWord={this.addWord} />
-        <div className="words">{words}</div>
+        <div className="words right-aligned">{badWords}</div>
+        <div className="words left-aligned">{goodWords}</div>
       </div>
     );
   }
